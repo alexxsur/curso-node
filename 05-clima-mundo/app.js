bb85e0da85b277300db70310638ae0a01643ef6a
Alex <alexxsur@gmail.com>
2019-11-29 22:11:35 -0500

    Peticiones axios - request

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +8,20 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+// https://rapidapi.com/dev132/api/city-geo-location-lookup
+
+const encodeURL = encodeURI(argv.direccion);
+console.log(encodeURL);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeURL }`,
+    headers: { 'x-rapidapi-key': '97a24c9530msha2372c0c0439c28p1bdf3djsn05a38a3fef6e' }
+});
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log('ERROR!!!!', err);
+    });
