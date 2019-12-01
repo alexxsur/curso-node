fab8d76b6efbaf43ab414597c5f9b4adb4ae4c77
Alex <alexxsur@gmail.com>
2019-12-01 14:52:22 -0500

    Open Weather Map

---
+++ 05-clima-mundo/clima/clima.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const getClima = async(lat, lng) => {
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=b2f973f82eb06d08c20f3f593065a311`)
+    return resp.data.main.temp;
+}
+
+module.exports = {
+    getClima
+}
