607c17ba59e0cdbd6d5de01a76675469ef0be979
Alex <alexxsur@gmail.com>
2019-12-01 20:14:44 -0500

    Conectando ambos servicios

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -13,6 +13,32 @@ const argv = require('yargs').options({
 /* lugar.getLugarLatLng(argv.direccion)
     .then(console.log); */
 
-clima.getClima(40.750000, -74.000000)
+/* clima.getClima(40.750000, -74.000000)
+    .then(console.log)
+    .catch(console.log); */
+
+const getInfo = async(direccion) => {
+
+    try {
+        //let coords = await lugar.getLugarLatLng(argv.direccion); DEBUG se comentatemporalmente debido a fallo de API rapidapi
+
+        /////// se agrega temporalmente para simular  respuesta dedbido a fallo de API rapidapi
+        coords = {
+                direccion: 'PRUEBA',
+                lat: 40.750000,
+                lng: -74.000000
+            }
+            //////
+
+        let temp = await clima.getClima(coords.lat, coords.lng);
+        return `El clima de ${coords.direccion} es de ${temp}.`;
+    } catch (error) {
+        return `No se pudo determinar el clima de ${direccion}`;
+    }
+
+
+}
+
+getInfo(argv.direccion)
     .then(console.log)
     .catch(console.log);
--- 05-clima-mundo/clima/clima.js
+++ 05-clima-mundo/clima/clima.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getClima = async(lat, lng) => {
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=b2f973f82eb06d08c20f3f593065a311`)
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=b2f973f82eb06d08c20f3f593065a311&units=metric`)
     return resp.data.main.temp;
 }
 
