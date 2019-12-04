8bab108fd5a21a6a30a800553d86b222dab75d21
Alex <alexxsur@gmail.com>
2019-12-03 19:32:12 -0500

    Introduccion a express

---
+++ 06-webserver/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    // res.send('Hello World')
+
+    let salida = {
+        nombre: 'Alex',
+        edad: 31,
+        url: req.url
+    }
+
+    res.send(salida);
+})
+
+app.get('/data', (req, res) => {
+
+    res.send('Hello Data')
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Escuchando peticiones en el puerto 3000");
+});
