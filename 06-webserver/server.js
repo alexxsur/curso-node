7082c72b525d27948ac84534b97df5b4b036a13d
Alex <alexxsur@gmail.com>
2019-12-11 22:10:55 -0500

    Usando parciales con HBS

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS Engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Alex',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
         anio: new Date().getFullYear()
     });
 });
