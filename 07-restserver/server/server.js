44260216937ffcaed904d198cf61e77d163b616e
Alex <alexxsur@gmail.com>
2019-12-15 12:11:42 -0500

    Usando Códigos de respuesta Http en Express

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -15,6 +15,18 @@ app.get('/usuario', function(req, res) {
 
 app.post('/usuario', function(req, res) {
     let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
     res.json({
         persona: body
     })
